8bbd4c8d2696c26e668d37ce3e0040f4b52d6e4d
Nikos Filippakis <aesmade@gmail.com>
2017-04-07 23:13:59 +0200

    main: count of ports seen
    * Adds a count of times seen per port.
    
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- main.go
+++ main.go
@@ -5,20 +5,37 @@ import (
     "github.com/nikofil/gopacketcache/lib"
 )
 
+type PortCounts map[gopacketcache.Port]uint
+
+func (portCounts PortCounts) String() string {
+    s := ""
+    for port, count := range portCounts {
+        s += fmt.Sprintf("\tPort %d: %d times\n", port, count)
+    }
+    return s
+}
+
 func main() {
     packetChannel, err := gopacketcache.OpenOffline("http.cap")
     if err == nil {
         i := 1
+        srcMap := make(PortCounts)
+        dstMap := make(PortCounts)
         for packet := range packetChannel {
             fmt.Printf("Packet %d: ", i)
             srcPort, errSrc := packet.SrcPort()
             dstPort, errDst := packet.DstPort()
             if errSrc == nil && errDst == nil {
                 fmt.Printf("Port %d -> %d\n", srcPort, dstPort)
+                srcMap[srcPort]++
+                dstMap[dstPort]++
             } else {
                 fmt.Println(err)
             }
             i++
         }
+        fmt.Println()
+        fmt.Println("Source ports count:\n", srcMap)
+        fmt.Println("Destination ports count:\n", dstMap)
     }
 }
